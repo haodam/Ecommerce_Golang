5150cfb2473b65920e2ff1e007acb979a0211280
dam hao <damanhhao3004@gmail.com>
2022-12-08 18:27:29 +0700

    update fw gin

--- main.go
+++ main.go
@@ -1,20 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"Ecommerce_Golang/component/appctx"
+	"Ecommerce_Golang/module/account/transport/ginaccount"
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"net/http"
 )
 
 func main() {
 
-	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+	dsn := "host=localhost user=root password=ecommerce123 dbname=ecommercegolang port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Println(db)
+	log.Println(db, err)
 
+	db = db.Debug()
+	appContext := appctx.NewAppContext(db)
+
+	r := gin.Default()
+	//r.Use(middleware.Recover(appContext))
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	v1 := r.Group("/v1")
+	account := v1.Group("/accounts")
+
+	account.POST("", ginaccount.CreateAccount(appContext))
+
+	r.Run()
 }
