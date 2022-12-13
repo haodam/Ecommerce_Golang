b749c19e2a010e9f88ff457d27534c11c5d9ff04
dam hao <damanhhao3004@gmail.com>
2022-12-13 18:12:39 +0700

    feat: feat add function list account for API

---
+++ module/account/biz/list_account.go
@@ -0,0 +1,31 @@
+package accountbiz
+
+import (
+	"Ecommerce_Golang/common"
+	accountmodel "Ecommerce_Golang/module/account/model"
+	"context"
+)
+
+type ListAccountStore interface {
+	ListDataWithCondition(ctx context.Context, filter *accountmodel.Filter, paging *common.Paging,
+		moreKeys ...string) ([]accountmodel.Account, error)
+}
+
+type listAccountBiz struct {
+	store ListAccountStore
+}
+
+func NewListRestaurantBiz(store ListAccountStore) *listAccountBiz {
+	return &listAccountBiz{store: store}
+}
+
+func (biz *listAccountBiz) ListAccount(ctx context.Context, filter *accountmodel.Filter, paging *common.Paging,
+	moreKeys ...string) ([]accountmodel.Account, error) {
+
+	result, err := biz.store.ListDataWithCondition(ctx, filter, paging)
+	if err != nil {
+		return nil, nil
+	}
+
+	return result, nil
+}
---
+++ module/account/transport/ginaccount/list_account.go
@@ -0,0 +1,10 @@
+package ginaccount
+
+import (
+	"Ecommerce_Golang/component/appctx"
+	"github.com/gin-gonic/gin"
+)
+
+func ListAccount(ctx appctx.AppContext) func(c *gin.Context) {
+	return nil
+}
