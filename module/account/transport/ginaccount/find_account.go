4c5bbffec9a5d9fa246509eec5dcdb9857773869
dam hao <damanhhao3004@gmail.com>
2022-12-13 18:13:28 +0700

    feat: feat add function find account for API

--- module/account/transport/ginaccount/find_account.go
+++ module/account/transport/ginaccount/find_account.go
@@ -1,1 +1,10 @@
 package ginaccount
+
+import (
+	"Ecommerce_Golang/component/appctx"
+	"github.com/gin-gonic/gin"
+)
+
+func FindAccount(ctx appctx.AppContext) func(c *gin.Context) {
+	return nil
+}
