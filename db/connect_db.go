63f1c9653299cf064672fd309e7a242c982728b3
dam hao <damanhhao3004@gmail.com>
2022-12-15 17:38:27 +0700

    fix: rename file db --> connect_db

---
+++ db/connect_db.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	accountmodel "Ecommerce_Golang/module/account/model"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
+func InitDB() *gorm.DB {
+
+	dsn := "host=localhost user=root password=ecommerce123 dbname=ecommercegolang port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(db, err)
+
+	db.AutoMigrate(&accountmodel.Account{})
+
+	return db
+}
